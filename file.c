3655f91b833806d67d34b61a6c2de775f2b48da1
лізонька <101859916+xxeliiq@users.noreply.github.com>
2022-06-03 01:20:09 +0200

    ^.^

---
+++ file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int turn = 0;
+main() {
+        pthread_t f2_thread, f1_thread;
+        void *f2(), *f1();
+        pthread_create(&f1_thread, NULL, f1, NULL);
+        pthread_create(&f2_thread, NULL, f2, NULL);
+        pthread_join(f1_thread, NULL);
+        pthread_join(f2_thread, NULL);
+}
+
+void *f1() {
+        while(1) {
+                while (turn!=0) ;
+                sleep(2);
+                printf("f1: critical Denysova!\n");
+                turn=1;
+                printf("f1: noncritical Liza!\n");
+        }
+}
+
+void *f2() {
+        while(1) {
+                sleep(1);
+                while (turn==0) ;
+                printf("f2: critical Denysova!\n");
+                sleep(3);
+                turn=0;
+                printf("f2: noncritical Liza!\n");
+        }
+}
